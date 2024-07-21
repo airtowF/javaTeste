70eb56f6816e82703d009a621dd44c7f528f7b3b
AirtowF <airtow@protonmail.com>
2024-07-21 20:18:16 -0300

    needless Locale method

--- Testes.java
+++ Testes.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class Testes {
 
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("en", "US"));
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
